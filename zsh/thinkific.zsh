1fdd04ce707a4d9361283e43ce1ff249c810a360
Shannon Fairchild <shannonfairchild@gmail.com>
2022-06-21 21:38:20 -0400

    add tmux

--- zsh/thinkific.zsh
+++ zsh/thinkific.zsh
@@ -1,4 +1,4 @@
 # Thinkific-specific Setup
 ssh-add --apple-load-keychain &> /dev/null
-export THINKIFICPATH=/Users/kelton/Thinkific
+export THINKIFICPATH=/Users/shannon/workspace
 export DEV_PACKAGES="zsh"
